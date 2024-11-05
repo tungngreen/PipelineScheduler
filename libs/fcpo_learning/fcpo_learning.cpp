7203d7c7b6ac7aa5fab2f48d9f71d750be718831
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-11-05 20:39:28 +0900

    fix: upper limit for update_steps

--- libs/fcpo_learning/fcpo_learning.cpp
+++ libs/fcpo_learning/fcpo_learning.cpp
@@ -127,8 +127,8 @@ void FCPOAgent::federatedUpdateCallback(FlData &response) {
     std::unique_lock<std::mutex> lock(model_mutex);
     torch::load(model, iss);
     steps_counter = 0;
-    update_steps += update_steps_inc; // Increase the number of steps for the next updates every time we finish federation
     federated_steps_counter = 1; // 1 means that we are starting local updates again until the next federation
+    if (update_steps < 300) update_steps += update_steps_inc; // Increase the number of steps for the next updates every time we finish federation
     reset();
 }
 
