d74a88b338c34cfb6fffae84f634320b5a09eb24
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-11-27 14:39:09 +0900

    fix: better rl analyze functions

--- analyze/rl_analyzes.py
+++ analyze/rl_analyzes.py
@@ -17,6 +17,7 @@ def reward_plot(directory):
     rewards = {}
     for agent in agents:
         rewards[agent] = []
+        if not os.path.isdir(os.path.join(directory, agent)): continue
         instances = natsorted(os.listdir(os.path.join(directory, agent)))
         for instance in instances:
             logs = natsorted(os.listdir(os.path.join(directory, agent, instance)))
@@ -45,10 +46,11 @@ def reward_plot(directory):
     plt.figure()
     for agent in agents:
         plt.plot(rewards[agent], label=agent)
-    plt.legend()
-    plt.title('Rewards')
-    plt.xlabel('Episodes')
-    plt.ylabel('Reward')
+    plt.legend(fontsize=12)
+    plt.title('Rewards', size=12)
+    plt.xlabel('Episodes', size=12)
+    plt.ylabel('Reward', size=12)
+    plt.ylim(0, 2)
     plt.show()
 
 def total_performance(directory):
@@ -58,39 +60,61 @@ def total_performance(directory):
     fig, axs = plt.subplots(1, 1, figsize=(8, 3), gridspec_kw={'height_ratios': [1], 'width_ratios': [1]})
     ax1 = axs
     cars = []
-    if include_people: people = []
-    # if the csvs do not exist, create them
-    if not os.path.exists(os.path.join(directory, 'df_cars.csv')) or not os.path.exists(
-            os.path.join(directory, 'df_people.csv')):
-        for f in natsorted(os.listdir(os.path.join(directory, 'OURS'))):
-            c, p, _, _ = read_file(os.path.join(directory, 'OURS', f))
-            if include_people: people.extend(p)
-            cars.extend(c)
-        df_cars = pd.DataFrame(cars, columns=['path', 'latency', 'timestamps'])
-        df_cars.to_csv(os.path.join(directory, 'df_cars.csv'), index=False)
-        if include_people:
-            df_people = pd.DataFrame(people, columns=['path', 'latency', 'timestamps'])
-            df_people.to_csv(os.path.join(directory, 'df_people.csv'), index=False)
-    else:
-        df_cars = pd.read_csv(os.path.join(directory, 'df_cars.csv'))
-        if include_people: df_people = pd.read_csv(os.path.join(directory, 'df_people.csv'))
-    lables = ['Traffic Throughput', 'Surveillance Throughput']
-    for j, df in enumerate([df_cars, df_people] if include_people else [df_cars]):
-        df['timestamps'] = df['timestamps'].apply(lambda x: int(x))
-        df['latency'] = df['latency'].apply(lambda x: int(x))
-        df['aligned_timestamp'] = (df['timestamps'] // 1e6).astype(int) * 1e6
-        df['throughput'] = df.groupby('aligned_timestamp')['latency'].transform('size')
-        df['aligned_timestamp'] = (df['aligned_timestamp'] - df['aligned_timestamp'].iloc[0]) / (60 * 1e6)
-        df = df.groupby('aligned_timestamp').agg({'throughput': 'mean'}).reset_index()
-        df = bucket_and_average(df, ['throughput'], num_buckets=780 if stepless else 80)
+    dfs_cars = {}
+    if include_people:
+        people = []
+        dfs_people = {}
+    for d in natsorted(os.listdir(directory)):
+        if not os.path.isdir(os.path.join(directory, d)): continue
+        # if the csvs do not exist, create them
+        if not os.path.exists(os.path.join(directory, d, 'df_cars.csv')) or not os.path.exists(
+                os.path.join(directory, d, 'df_people.csv')):
+                for f in natsorted(os.listdir(os.path.join(directory, d))):
+                    c, p, _, _ = read_file(os.path.join(directory, d, f))
+                    if include_people: people.extend(p)
+                    cars.extend(c)
+                df_cars = pd.DataFrame(cars, columns=['path', 'latency', 'timestamps'])
+                df_cars.to_csv(os.path.join(directory, d, 'df_cars.csv'), index=False)
+                dfs_cars[d] = df_cars
+                if include_people:
+                    df_people = pd.DataFrame(people, columns=['path', 'latency', 'timestamps'])
+                    df_people.to_csv(os.path.join(directory, d, 'df_people.csv'), index=False)
+                    dfs_people[d] = df_people
+        else:
+            df_cars = pd.read_csv(os.path.join(directory, d, 'df_cars.csv'))
+            dfs_cars[d] = df_cars
+            if include_people:
+                df_people = pd.read_csv(os.path.join(directory, d, 'df_people.csv'))
+                dfs_people[d] = df_people
+    for j, d in enumerate(natsorted(os.listdir(directory))):
+        dfs_cars[d]['timestamps'] = dfs_cars[d]['timestamps'].apply(lambda x: int(x))
+        dfs_cars[d]['latency'] = dfs_cars[d]['latency'].apply(lambda x: int(x))
+        dfs_cars[d]['aligned_timestamp'] = (dfs_cars[d]['timestamps'] // 1e6).astype(int) * 1e6
+        dfs_cars[d]['throughput'] = dfs_cars[d].groupby('aligned_timestamp')['latency'].transform('size')
+        dfs_cars[d]['aligned_timestamp'] = (dfs_cars[d]['aligned_timestamp'] - dfs_cars[d]['aligned_timestamp'].iloc[0]) / (60 * 1e6)
+        dfs_cars[d] = dfs_cars[d].groupby('aligned_timestamp').agg({'throughput': 'mean'}).reset_index()
+        dfs_cars[d] = bucket_and_average(dfs_cars[d], ['throughput'], num_buckets=780 if stepless else 80)
         # remove values before 10 and offset the time for the remaining values
-        df = df[df['aligned_timestamp'] > 11]
-        df['aligned_timestamp'] -= 11
-        ax1.plot(df['aligned_timestamp'], df['throughput'], label=lables[j], color=colors[j], linewidth=3)
+        dfs_cars[d] = dfs_cars[d][dfs_cars[d]['aligned_timestamp'] > 11]
+        dfs_cars[d]['aligned_timestamp'] -= 11
+        ax1.plot(dfs_cars[d]['aligned_timestamp'], dfs_cars[d]['throughput'], label=d + ' Traffic', color=colors[j], linewidth=3)
+        if include_people:
+            if d == 'OURS': j = 2
+            dfs_people[d]['timestamps'] = dfs_people[d]['timestamps'].apply(lambda x: int(x))
+            dfs_people[d]['latency'] = dfs_people[d]['latency'].apply(lambda x: int(x))
+            dfs_people[d]['aligned_timestamp'] = (dfs_people[d]['timestamps'] // 1e6).astype(int) * 1e6
+            dfs_people[d]['throughput'] = dfs_people[d].groupby('aligned_timestamp')['latency'].transform('size')
+            dfs_people[d]['aligned_timestamp'] = (dfs_people[d]['aligned_timestamp'] - dfs_people[d]['aligned_timestamp'].iloc[0]) / (60 * 1e6)
+            dfs_people[d] = dfs_people[d].groupby('aligned_timestamp').agg({'throughput': 'mean'}).reset_index()
+            dfs_people[d] = bucket_and_average(dfs_people[d], ['throughput'], num_buckets=780 if stepless else 80)
+            # remove values before 10 and offset the time for the remaining values
+            dfs_people[d] = dfs_people[d][dfs_people[d]['aligned_timestamp'] > 11]
+            dfs_people[d]['aligned_timestamp'] -= 11
+            ax1.plot(dfs_people[d]['aligned_timestamp'], dfs_people[d]['throughput'], label=d + ' Surveillance', color=colors[j], linewidth=3, linestyle='--')
     ax1.set_title('Throughput over 8h', size=12)
     ax1.set_ylabel('Throughput (objects / s)', size=12)
     ax1.set_xlabel('Minutes Passed since Start (min)', size=12)
     ax1.set_xlim([0, 480])
-    ax1.legend(loc='lower left', fontsize=12)
+    ax1.legend(fontsize=12)
     plt.tight_layout()
     plt.show()
