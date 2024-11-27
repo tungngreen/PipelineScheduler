262bb73df7e7b8945fd8319501c8ec8d86582abf
Lucas Liebe <lucasliebe.ll@gmail.com>
2024-11-27 15:48:55 +0900

    fix: optimize reward weights

--- libs/fcpo_learning/fcpo_learning.cpp
+++ libs/fcpo_learning/fcpo_learning.cpp
@@ -164,8 +164,8 @@ void FCPOAgent::rewardCallback(double throughput, double drops, double latency_p
         first = false;
         return;
     }
-    double reward = 4 * throughput - 10 * latency_penalty - 2 * oversize_penalty;
-    reward = std::max(-10.0, std::min(10.0, reward)) / 10.0; // Clip reward to [-10, 10] and then scale it to [-1, 1]
+    double reward = (1.1 * throughput - 10 * latency_penalty - 2 * std::pow(oversize_penalty, 2)) / 2.0;
+    reward = std::max(-1.0, std::min(1.0, reward)); // Clip reward to [-1, 1]
     experiences.add_reward(reward);
     cumu_reward += reward;
 }
