6bd3947227dc3fddbbb67c79ed8f900d6c3109fe
Lucas Liebe <lucasliebe.ll@gmail.com>
2025-10-02 11:07:07 +0900

    fix: SinkAgent with zmq

--- libs/device_agent/sink_agent/sink_agent.cpp
+++ libs/device_agent/sink_agent/sink_agent.cpp
@@ -1,6 +1,13 @@
 #include "sink_agent.h"
 
 SinkAgent::SinkAgent(const std::string &controller_url) : DeviceAgent() {
+    controller_ctx = context_t(1);
+    std::string server_address = absl::StrFormat("tcp://%s:%d", controller_url, CONTROLLER_MESSAGE_QUEUE_PORT + dev_system_port_offset  - dev_agent_port_offset);
+    controller_message_queue = socket_t(controller_ctx, ZMQ_SUB);
+    controller_message_queue.setsockopt(ZMQ_SUBSCRIBE, (dev_name + "|").c_str(), dev_name.size() + 1);
+    controller_message_queue.connect(server_address);
+    controller_message_queue.set(zmq::sockopt::rcvtimeo, 1000);
+
     dev_logPath += "/sink_agent";
     std::filesystem::create_directories(
             std::filesystem::path(dev_logPath)
