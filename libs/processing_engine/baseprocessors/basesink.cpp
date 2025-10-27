1a00f2c05fdf99e71fd1c254cc454a2c28853951
Lucas Liebe <lucasliebe.ll@gmail.com>
2025-10-27 14:30:19 +0900

    feat: log final data matrix in sink

--- libs/processing_engine/baseprocessors/basesink.cpp
+++ libs/processing_engine/baseprocessors/basesink.cpp
@@ -87,16 +87,18 @@ void BaseSink::sink() {
         } else if (msvc_RUNMODE == RUNMODE::DEPLOYMENT || msvc_RUNMODE == RUNMODE::PROFILING) {
             spdlog::get("container_agent")->trace("{0:s} is processing request {1:s}...", msvc_name, inferTimeReport.req_travelPath[0]);
             msvc_logFile << inferTimeReport.req_travelPath[0] << "|";
-            for (unsigned int j = 0; j < inferTimeReport.req_origGenTime[0].size() - 1; j++) {
+            for (unsigned int j = 0; j < inferTimeReport.req_origGenTime[0].size() - 1; j++)
                 msvc_logFile << timePointToEpochString(inferTimeReport.req_origGenTime[0].at(j)) << ",";
-            }
             msvc_logFile << timePointToEpochString(inferTimeReport.req_origGenTime[0].back()) << "|";
 
-            for (BatchSizeType j = 1; j < inferTimeReport.req_origGenTime[0].size(); j++) {
+            for (unsigned int j = 1; j < inferTimeReport.req_origGenTime[0].size(); j++)
                 msvc_logFile << std::chrono::duration_cast<TimePrecisionType>(inferTimeReport.req_origGenTime[0].at(j) - inferTimeReport.req_origGenTime[0].at(j-1)).count() << ",";
-            }
-            msvc_logFile << std::chrono::duration_cast<TimePrecisionType>(inferTimeReport.req_origGenTime[0].back() - inferTimeReport.req_origGenTime[0].front()).count() << std::endl;
+            msvc_logFile << std::chrono::duration_cast<TimePrecisionType>(inferTimeReport.req_origGenTime[0].back() - inferTimeReport.req_origGenTime[0].front()).count() << "|";
+            for (BatchSizeType j = 0; j < inferTimeReport.req_data.size() - 1; j++)
+                msvc_logFile << inferTimeReport.req_data[j].data << ",";
+            msvc_logFile << inferTimeReport.req_data.back().data;
             agg_Latency += std::chrono::duration_cast<TimePrecisionType>(inferTimeReport.req_origGenTime[0].at(1) - inferTimeReport.req_origGenTime[0].at(0)).count();
+            msvc_logFile << std::endl;
         }
     }
     msvc_logFile.close();
